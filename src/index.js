61c02f8c8b5382b87b34b27cc965a0b086c2c59e
ge73xeq <moiez.syed@tum.de>
2021-10-17 18:59:06 +0200

    removed unnecessary comment

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// The <App /> is just a JS function, which displays the REACT logo onto the screen
+// The <App /> is just a react component, which displays the REACT logo onto the screen
 // This can be changed to input any HTML element
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {/* <h1>Hello World!</h1> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
