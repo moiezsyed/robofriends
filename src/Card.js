5d359181cd103378474552507ffe9448c0c9c6cf
ge73xeq <moiez.syed@tum.de>
2021-10-18 14:08:39 +0200

    added new component 'Card'

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <img src=" https://robohash.org/blabla" alt="robot"></img>
+            <div>
+                <h2>John Doe</h2>
+                <p>John.Doe@robot.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
