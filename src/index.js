68c6217cf6f8f2d8a5b077e3ab5552450dd77867
ge73xeq <moiez.syed@tum.de>
2021-10-18 12:34:26 +0200

    modified index to contain new component Hello

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // notice here that we didn't metnion "App.js"; because by default files without extensions are assumed to be JS files
 import App from './App';
+import Hello from "./Hello"
 import reportWebVitals from './reportWebVitals';
 
 // The <App /> is just a react component, which displays the REACT logo onto the screen
-// This can be changed to input any HTML element
+// This can be changed to input any HTML element or component
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
