cebd39396bd30bac0b97876fbd8db0618ab51b34
ge73xeq <moiez.syed@tum.de>
2021-10-18 12:33:56 +0200

    added test component

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Hello extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <p>React Ninja I Am</p>
+            </div>
+        );
+    }
+}
+
+// export default keywords are used to export only one class or function at a time
+export default Hello;
