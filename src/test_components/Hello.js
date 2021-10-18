3de57c4d79a6861d8131df35212c480bb3d362e2
ge73xeq <moiez.syed@tum.de>
2021-10-18 13:58:05 +0200

    added return documentation

--- src/test_components/Hello.js
+++ src/test_components/Hello.js
@@ -3,6 +3,7 @@ import './Hello.css'
 
 class Hello extends React.Component{
     render() {
+        // return has to return one element, in this case "div"; which can contain other elements inside it
         return (
             // using tachyons directly in the JS file, by using "className" and not "class", as this is a JSX format and not CSS
             <div className='f1 tc'>
