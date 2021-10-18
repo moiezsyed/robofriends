ffc18cd9185a4f686836566d0465dc1ad9529cc9
ge73xeq <moiez.syed@tum.de>
2021-10-18 15:02:27 +0200

    contains robot information

---
+++ src/robots.js
@@ -0,0 +1,23 @@
+// This is the way to export multiple objects/functions, if available
+export const robots = [
+    {
+        name: "John Doe",
+        email: "John.Doe@gmail.com",
+        hobby: "Gardening",
+        id: 5472
+    },
+    {
+        name: "Will Smith",
+        email: "Will.Smith@gmail.com",
+        hobby: "Acting",
+        id: 6621
+    },
+    {
+        name: "Tommy Fury",
+        email: "Tommy.Fury@gmail.com",
+        hobby: "Boxing",
+        id: 3595
+    }
+];
+
+// here id's are assigned in order to change the robot image for each Card component
