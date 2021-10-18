42aa3af1067c7f421bc9aafd85db6c315da57486
ge73xeq <moiez.syed@tum.de>
2021-10-18 19:12:03 +0200

    documentation on return prop

--- src/CardList.js
+++ src/CardList.js
@@ -7,6 +7,7 @@ const CardList = (props) => {
     // use map method to loop over robots array (could also use forEach method)
     const cardComponent = robots.map((robot, index) => {
         // Card Component is called here (necessary to give exact names of props as used in the "Card" component)
+        // "key" prop here is needed so that the virtual DOM recognizes each individual Card component from the other; without the "key" prop, the developer tool gives an error, and virtual DOM would have to erase everything if a Card component is deleted
         return (<Card key={index} name={robot.name} email={robot.email} hobby={robot.hobby} 
         id={robot.id} /> );
     })
