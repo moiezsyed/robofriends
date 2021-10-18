9315434c9355704be898d34e584b9ab116776654
ge73xeq <moiez.syed@tum.de>
2021-10-18 19:06:13 +0200

    added comments regarding return statement of our component

--- src/CardList.js
+++ src/CardList.js
@@ -1,6 +1,21 @@
 import React from "react"
+import Card from "./Card"
 
-const CardList = (robots) => {
+// NOTE: could directly destructure our "robots" prop inside the argument to directly access the array
+const CardList = (props) => {
+    const robots = props.robots;
+    // use map method to loop over robots array (could also use forEach method)
+    const cardComponent = robots.map((robot, index) => {
+        // Card Component is called here (necessary to give exact names of props as used in the "Card" component)
+        return (<Card key={index} name={robot.name} email={robot.email} hobby={robot.hobby} 
+        id={robot.id} /> );
+    })
+
+    return(
+        // only one return statement, so we don't need <div></div>
+        // if wrapped in <div></div>, then we need to wrap cardComponent in curly braces, as JS object
+        cardComponent
+    );
 }
 
 export default CardList;
