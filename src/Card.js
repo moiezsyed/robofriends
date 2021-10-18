3b4d8460304ceef56236a4b24ee2f4de06637d24
ge73xeq <moiez.syed@tum.de>
2021-10-18 17:25:05 +0200

    added styling using tachyons

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,8 @@ const Card = (props) => {
     // destructuring props, which is a JSX object containing name, email, hobby & id in that order we listed in "index.js"
     const {name, email, hobby, id} = props;
     return (
-        <div>
+        // "dib" tachyon property is used to get the items inline, i.e. on one line
+        <div className="bg-light-purple ba b--transparent br3 grow ma2 measure tc pa3 small-caps calisto light-green hover-black shadow-hover contain dib">
             <img src={`https://robohash.org/${id}`} alt="robot"></img>
             <div>
                 {/* every time Card component is called in the "index.js" file, it's properties(props) will be read and used here */}
