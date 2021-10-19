7bb6c53daf654248bd3361d165310791848b1d8a
ge73xeq <moiez.syed@tum.de>
2021-10-19 22:53:32 +0200

    added documentation

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ import Searchbox from "./Searchbox"
 // multiple objects/functions can be imported by using curly braces
 import {robots} from "./robots"
 
+// created class "App", which is parent class to all other component
+// all other classes, in this case "Searchbox" and "CardList" communicate through this parent class
+// "App" has states, which define our React code and are passed along to the child components as "props", i.e. all states can be used as props. It's like a binary tree structure, with the parent class ("App") being the root node
 class App extends React.Component{
     constructor() {
+        // takes in the constructor from the React.component class
         super()
+        // describing the state of our React code (always done in the parent class, so the children can use it)
         this.state = {
             robots: robots,
             searchfield: ''
@@ -17,9 +22,11 @@ class App extends React.Component{
     };
 
     // onChange is an event handler; where the events are input to the handler by means of a class method in this case
+    // in this case the event is to listen to the keyboard changes from the user; used in the "Searchbox" component using "onChange" attribute
     onSearchChange = (event) => {
+        // setState changes the current state of the event to the new state that the user inputs
         this.setState({searchfield: event.target.value})
-        console.log(event);
+        console.log(event.target.value);
     };
 
     render() {
--- src/Searchbox.js
+++ src/Searchbox.js
@@ -4,7 +4,7 @@ const Searchbox = ({change}) => {
     return (
         <div className="pa1 ma2">
             {/* https://upmostly.com/tutorials/react-onchange-events-with-examples */}
-            {/* onChange is an event handler; where the events are input to the handler by means of a class method in this case */}
+            {/* onChange takes as input the event with all the events data such as name, value etc. */}
             <input className="bg-pink " type="search" placeholder="enter robot name" onChange={change}/>      
         </div>
     );
