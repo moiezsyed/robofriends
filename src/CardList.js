a080c95273731415014e57cd3105d3140c821373
ge73xeq <moiez.syed@tum.de>
2021-10-19 23:56:19 +0200

    minor changes

--- src/CardList.js
+++ src/CardList.js
@@ -7,10 +7,10 @@ const CardList = ({robots}) => {
     const cardComponent = robots.map((robot, index) => {
         // Card Component is called here (necessary to give exact names of props as used in the "Card" component)
         // "key" prop here is needed so that the virtual DOM recognizes each individual Card component from the other; without the "key" prop, the developer tool gives an error, and virtual DOM would have to erase everything if a Card component is deleted
-        return (<Card key={index} name={robot.name} email={robot.email} hobby={robot.hobby} 
+        return (<Card key={index} name={robot.name} email={robot.email} profession={robot.profession} 
         id={robot.id} /> );
     })
-
+    
     return(
         // only one return statement, so we don't need <div></div>
         // if wrapped in <div></div>, then we need to wrap cardComponent in curly braces, as JS object
