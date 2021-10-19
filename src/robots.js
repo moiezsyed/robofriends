dc0d38fdc1a8d185db842cf08305cc11dad223d7
ge73xeq <moiez.syed@tum.de>
2021-10-19 17:44:17 +0200

    added more robots

--- src/robots.js
+++ src/robots.js
@@ -3,21 +3,63 @@ export const robots = [
     {
         name: "John Doe",
         email: "John.Doe@gmail.com",
-        hobby: "Gardening",
+        profession: "Gardening",
         id: 5472
     },
     {
         name: "Will Smith",
         email: "Will.Smith@gmail.com",
-        hobby: "Acting",
+        profession: "Actor",
         id: 6621
     },
     {
         name: "Tommy Fury",
         email: "Tommy.Fury@gmail.com",
-        hobby: "Boxing",
+        profession: "Boxer",
         id: 3595
-    }
+    },
+    {
+        name: "Madam Teresa",
+        email: "Madam.Teresa@gmail.com",
+        profession: "Social Work",
+        id: 4042
+    },
+    {
+        name: "Kofi Anan",
+        email: "Kofi.Anan@gmail.com",
+        profession: "UN Chief",
+        id: 3334
+    },
+    {
+        name: "The Pope",
+        email: "The.Pope@vatican.it",
+        profession: "Religious Leader",
+        id: 6666
+    },
+    {
+        name: "Barack Obama",
+        email: "Barack.Obama@usa.gov",
+        profession: "Leader",
+        id: 1111
+    },
+    {
+        name: "Lenny Kravitz",
+        email: "Lenny.Kravitz@gmail.com",
+        profession: "Musician",
+        id: 9853
+    },
+    {
+        name: "Johnny Depp",
+        email: "Johnny.Depp@gmail.com",
+        profession: "Actor",
+        id: 1988
+    },
+    {
+        name: "Shakira",
+        email: "Shakira@yahoo.com",
+        profession: "Entertainer",
+        id: 1597
+    },
 ];
 
 // here id's are assigned in order to change the robot image for each Card component
