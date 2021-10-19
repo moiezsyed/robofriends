7258d77b72e881df0db7825cb331b1ca4a3fdda8
ge73xeq <moiez.syed@tum.de>
2021-10-19 23:56:51 +0200

    added more stylings to the Searchbox component

--- src/Searchbox.js
+++ src/Searchbox.js
@@ -5,7 +5,7 @@ const Searchbox = ({change}) => {
         <div className="pa1 ma2">
             {/* https://upmostly.com/tutorials/react-onchange-events-with-examples */}
             {/* onChange takes as input the event with all the events data such as name, value etc. */}
-            <input className="bg-pink " type="search" placeholder="enter robot name" onChange={change}/>      
+            <input className="bg-pink pa2 grow" type="search" placeholder="enter robot name..." onChange={change}/>
         </div>
     );
 }
