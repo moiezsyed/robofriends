2ceebdf2049ba095625a1cbd75a07aff322b9a07
ge73xeq <moiez.syed@tum.de>
2021-10-19 23:55:42 +0200

    updated to add filteredRobot in the render

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ class App extends React.Component{
 
     // onChange is an event handler; where the events are input to the handler by means of a class method in this case
     // in this case the event is to listen to the keyboard changes from the user; used in the "Searchbox" component using "onChange" attribute
+    // always create methods in classes using the "arrow function" to avoid unnecesssary errors
     onSearchChange = (event) => {
         // setState changes the current state of the event to the new state that the user inputs
         this.setState({searchfield: event.target.value})
@@ -30,11 +31,19 @@ class App extends React.Component{
     };
 
     render() {
+        // filter out the robots when searched for their names
+        const filteredRobot = this.state.robots.filter((robot) => {
+            // when returns true, it will return that robot details
+            // https://www.w3schools.com/jsref/jsref_includes.asp
+            // https://www.w3schools.com/jsref/jsref_tolowercase.asp
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        });
+
         return (
             <div className="tc">
                 <h1>RoboFriends</h1>
                 <Searchbox change={this.onSearchChange}/>
-                <CardList robots={this.state.robots} />
+                <CardList robots={filteredRobot} />
             </div>
         )
     };
