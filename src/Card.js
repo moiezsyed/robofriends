10e9ad2450939fd5ff475856d3aad93e64d7d00c
ge73xeq <moiez.syed@tum.de>
2021-10-20 12:29:31 +0200

    changed individual components color

--- src/Card.js
+++ src/Card.js
@@ -3,16 +3,16 @@ import React from "react";
 const Card = (props) => {
     // destructuring props, which is a JSX object containing name, email, hobby & id properties
     // props gives us all the properties used in the component
-    const {name, email, hobby, id} = props;
+    const {name, email, profession, id} = props;
     return (
         // "dib" tachyon property is used to get the items inline, i.e. on one line
-        <div className="bg-light-purple ba b--transparent br3 grow ma2 measure tc pa3 small-caps calisto light-green hover-black shadow-hover contain dib">
+        <div className="bg-dark-purple ba b--transparent br3 grow ma2 measure tc pa3 small-caps calisto light-green hover-black shadow-hover contain dib">
             <img src={`https://robohash.org/${id}`} alt="robot"></img>
             <div>
                 {/* every time Card component is called in the "index.js" file, it's properties(props) will be read and used here */}
                 <h2>{name}</h2>
                 <p>{email}</p>
-                <p>{hobby}</p>
+                <p>{profession}</p>
             </div>
         </div>
     )
