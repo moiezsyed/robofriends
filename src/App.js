f1263d2c26511f8322ea9c030dfa8e23b69c57aa
ge73xeq <moiez.syed@tum.de>
2021-10-20 14:45:19 +0200

    minor fix

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@
 import React from "react"
 import CardList from "./CardList"
 import Searchbox from "./Searchbox"
-// multiple objects/functions can be imported by using curly braces
-import {robots} from "./robots"
 import "./App.css"
 
 // created class "App", which is parent class to all other component
+// classes that have state are known as "smart components"
 // all other classes, in this case "Searchbox" and "CardList" communicate through this parent class
 // "App" has states, which define our React code and are passed along to the child components as "props", i.e. all states can be used as props. It's like a binary tree structure, with the parent class ("App") being the root node
 class App extends React.Component{
@@ -17,14 +16,14 @@ class App extends React.Component{
         super()
         // describing the state of our React code (always done in the parent class, so the children can use it)
         this.state = {
-            robots: robots,
+            robots: []],
             searchfield: ''
         };
     };
 
     // onChange is an event handler; where the events are input to the handler by means of a class method in this case
     // in this case the event is to listen to the keyboard changes from the user; used in the "Searchbox" component using "onChange" attribute
-    // always create methods in classes using the "arrow function" to avoid unnecesssary errors
+    // always create methods in classes using the "arrow function" to avoid unnecesssary errors, except pre-defined React methods
     onSearchChange = (event) => {
         // setState changes the current state of the event to the new state that the user inputs
         this.setState({searchfield: event.target.value})
