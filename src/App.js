6e4ed93a429ea9ee5c12f77c772d57053c6298ed
ge73xeq <moiez.syed@tum.de>
2021-10-20 15:51:57 +0200

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import "./App.css"
 // classes that have state are known as "smart components"
 // all other classes, in this case "Searchbox" and "CardList" communicate through this parent class
 // "App" has states, which define our React code and are passed along to the child components as "props", i.e. all states can be used as props. It's like a binary tree structure, with the parent class ("App") being the root node
-class App extends React.Component{
+class App extends React.Component {
     constructor() {
         // takes in the constructor from the React.component class
         super()
@@ -19,6 +19,7 @@ class App extends React.Component{
             robots: [],
             searchfield: ''
         };
+        console.log("constructor");
     };
 
     // onChange is an event handler; where the events are input to the handler by means of a class method in this case
@@ -26,24 +27,29 @@ class App extends React.Component{
     // always create methods in classes using the "arrow function" to avoid unnecesssary errors, except pre-defined React methods
     onSearchChange = (event) => {
         // setState changes the current state of the event to the new state that the user inputs
-        this.setState({searchfield: event.target.value})
+        this.setState({ searchfield: event.target.value })
         console.log(event.target.value);
     };
 
     componentDidMount() {
+        console.log("componentDidMount");
+        // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
         fetch("https://jsonplaceholder.typicode.com/users")
-            // we have to return the response in json format
+            // we have to return the response in json format & the response is returned only when the "promise" is fulfilled
             .then(response => {
+                // additional command to check for HTTP errors if any
+                console.log(response.status);
                 return response.json();
             })
             // we're getting users from the API by updating the state to the users from the API
             // if this is not done, we don't get the users
             .then(users => {
-                this.setState({robots: users})
+                this.setState({ robots: users })
             });
     }
 
     render() {
+        console.log("render");
         // filter out the robots when searched for their names
         // this function links the two components
         const filteredRobot = this.state.robots.filter((robot) => {
@@ -53,14 +59,19 @@ class App extends React.Component{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         });
 
-        return (
-            <div className="tc">
-                <h1 className="f1 light-green"> RoboFriends</h1>
-                <Searchbox change={this.onSearchChange}/>
-                <CardList filteredRobot={filteredRobot} />
-            </div>
-        )
-    };
+        // if-clause is used in the case it takes a while to fetch the response from the API
+        if (this.state.robots.length === 0){
+            return <h1 className="f1 light-green tc">Loading...</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1 light-green"> RoboFriends</h1>
+                    <Searchbox change={this.onSearchChange} />
+                    <CardList filteredRobot={filteredRobot} />
+                </div>
+            );
+        }
+    }
 }
 
 export default App;
