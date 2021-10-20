10b974e2e20114ebe47a09c7c33aab2afc97a7b7
ge73xeq <moiez.syed@tum.de>
2021-10-20 15:25:05 +0200

    updated variable names and on show information

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = (props) => {
     // destructuring props, which is a JSX object containing name, email, hobby & id properties
     // props gives us all the properties used in the component
-    const {name, email, profession, id} = props;
+    const {name, email, website, id} = props;
     return (
         // "dib" tachyon property is used to get the items inline, i.e. on one line
         <div className="bg-dark-purple ba b--transparent br3 grow ma2 measure tc pa3 small-caps calisto light-green hover-black shadow-hover contain dib">
@@ -12,7 +12,7 @@ const Card = (props) => {
                 {/* every time Card component is called in the "index.js" file, it's properties(props) will be read and used here */}
                 <h2>{name}</h2>
                 <p>{email}</p>
-                <p>{profession}</p>
+                <p>{website}</p>
             </div>
         </div>
     )
